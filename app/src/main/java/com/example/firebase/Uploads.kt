5323d3b98c3acb821918f519ef3e3a0d256adec7
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-10-10 14:15:30 +0700

    Uploads

---
+++ app/src/main/java/com/example/firebase/Uploads.kt
@@ -0,0 +1,17 @@
+package com.example.firebase
+
+class Uploads {
+    var nama : String = nama
+        get() = field
+        set(value) {
+            field = value
+        }
+
+
+    var image : String = image
+        get() = field
+        set(value) {
+            field = value
+
+        }
+}
